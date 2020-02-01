4f58cf2a641053acdcaf63a49701df48692d716d
Samuel Tijani <omalitijani@gmail.com>
2020-02-01 16:25:00 +0100

    Fix #1

--- e-SuperMartApp-FrontEnd/src/app/app-routing.module.ts
+++ e-SuperMartApp-FrontEnd/src/app/app-routing.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent
+  },
+  {
+    path: 'products',
+    pathMatch: 'full',
+    loadChildren: () => import('./products/products.module').then(m => m.ProductsModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
