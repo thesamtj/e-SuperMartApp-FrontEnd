cbb574b3d67d065facede5973c5d3d16ad9d5053
Samuel Tijani <omalitijani@gmail.com>
2020-02-01 20:07:47 +0100

    Add material design

--- e-SuperMartApp-FrontEnd/src/app/app.component.ts
+++ e-SuperMartApp-FrontEnd/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,9 +6,11 @@ import { Router } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   
   constructor() {}
 
- 
+  ngOnInit() {
+
+  }
 }
