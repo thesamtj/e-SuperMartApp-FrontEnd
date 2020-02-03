83ec1c1161a017c57bc8aa26bcefd64b4bdd3b3b
Samuel Tijani <omalitijani@gmail.com>
2020-02-03 17:37:46 +0100

    Fix the toolbar menu

--- e-SuperMartApp-FrontEnd/src/app/app-routing.module.ts
+++ e-SuperMartApp-FrontEnd/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 
@@ -24,6 +25,10 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
   }
 ];
 
--- e-SuperMartApp-FrontEnd/src/app/register/register.component.ts
+++ e-SuperMartApp-FrontEnd/src/app/register/register.component.ts
@@ -12,8 +12,7 @@ export class RegisterComponent implements OnInit {
   userForm = new FormGroup({
     fullname: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required]),
-    repeatPassword: new FormControl('', [Validators.required, this.passwordsMatch])
+    password: new FormControl('', [Validators.required])
   });
   
   constructor(private router: Router, 
@@ -24,40 +23,13 @@ export class RegisterComponent implements OnInit {
   ngOnInit() {
   }
 
-  passwordsMatch(control: FormControl) {
-    let password = control.root.get('password');
-    return password && control.value !== password.value
-        ? {
-            passwordMatch: true
-          }
-        : null;
-  }
-
   register() {
-    if (!this.userForm.valid) {
-      return;
-    }
-
+    
     const user = this.userForm.getRawValue();
     this.authService
       .register(user)
       .subscribe(s=> this.router.navigate(['/login']));
   }
 
-  get fullname() {
-    return this.userForm.get('fullname');
-  }
-
-  get email() {
-    return this.userForm.get('email');
-  }
-
-  get password() {
-    return this.userForm.get('password');
-  }
-
-  get repeatPassword() {
-    return this.userForm.get('repeatPassword');
-  }
-
+  
 }
