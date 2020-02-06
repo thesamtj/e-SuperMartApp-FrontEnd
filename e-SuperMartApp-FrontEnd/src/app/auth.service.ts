5a441ade5ecbd5fa418555a21383d622362112ae
Samuel Tijani <omalitijani@gmail.com>
2020-02-06 18:59:13 +0100

    fix #19

--- e-SuperMartApp-FrontEnd/src/app/auth.service.ts
+++ e-SuperMartApp-FrontEnd/src/app/auth.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { of } from 'rxjs';
+import { of, Subject, throwError, EMPTY } from 'rxjs';
+import { switchMap, catchError } from 'rxjs/operators';
 import { User } from './user';
 import { TokenStorageService } from './token.storage.service';
 
+interface UserDto {
+    user: User;
+    token: string;
+}
+  
 @Injectable({
   providedIn: 'root'
 })
@@ -16,26 +22,33 @@ export class AuthService {
    constructor(private httpClient: HttpClient, 
     private tokenStorage: TokenStorageService) { }
 
-   login(email: string, password: string) {
-    const loginCredentials = { email, password };
-    return this.httpClient.post<User>(`${this.apiUrl}login`, loginCredentials).pipe(
-      switchMap(foundUser => {
-        this.setUser(foundUser);
-        console.log('user found', foundUser);
-        return of(foundUser);
-      }),
-      catchError(e=> {
-        return throwError('Login could not be verified. Please try again.');
-      })
-    );
-  } 
+    login(email: string, password: string) {
+        const loginCredentials = { email, password };
+        return this.httpClient.post<UserDto>(`${this.apiUrl}login`, loginCredentials).pipe(
+          switchMap(({ user, token }) => {
+            this.setUser(user);
+            this.tokenStorage.setToken(token);
+            console.log('user found', user);
+            return of(user);
+          }),
+          catchError(e=> {
+            this.logService.log(`Server error occured ${e.error}`, e);
+            return throwError('Login could not be verified. Please try again.');
+          })
+        );
+      }
 
   logout() {
+    // remove user from suject
+    // remove token from localstorage
+
+    this.tokenStorage.removeToken();
     this.setUser(null);
     console.log("user did logout successfull");
   }
 
   get user() {
+      
     this.user$.asObservable();
   }
 
@@ -54,8 +67,33 @@ export class AuthService {
     );
   }
 
+  findMe() {
+    const token = this.tokenStorage.getToken();
+    if (!token) {
+      return EMPTY;
+    }
+
+    return this.httpClient.get<any>(`${this.apiUrl}findme`).pipe(
+      switchMap(({ user }) => {
+        this.setUser(user);
+        console.log(`user found`, user);
+        return of(user);
+      }),
+      catchError(e => {
+        console.log(
+          `Your login details could not be verified. Please try again`,
+          e
+        );
+        return throwError(
+          `Your login details could not be verified. Please try again`
+        );
+      })
+    );
+  }
+
   private setUser(user) {
     this.user$.next(user);
   }
+
   
 }
