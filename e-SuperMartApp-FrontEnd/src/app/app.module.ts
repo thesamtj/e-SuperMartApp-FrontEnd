067d5a5da676fb1ccefb0110b8780ac15ab946ac
Samuel Tijani <omalitijani@gmail.com>
2020-02-07 09:49:35 +0100

    Fix #23

--- e-SuperMartApp-FrontEnd/src/app/app.module.ts
+++ e-SuperMartApp-FrontEnd/src/app/app.module.ts
@@ -4,14 +4,13 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppComponent } from "./app.component";
-import { PmMaterialModule } from "./material-module";
 import { HomeComponent } from "./home/home.component";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from './register/register.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthHeaderInterceptorService } from './interceptors/auth-header-interceptor.service';
+import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, LoginComponent, RegisterComponent],
@@ -19,9 +18,7 @@ import { AuthHeaderInterceptorService } from './interceptors/auth-header-interce
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    PmMaterialModule,
-    FormsModule,
-    ReactiveFormsModule,
+    SharedModule
     HttpClientModule
   ],
   providers: [{
