6f1675848adeebc26bf66f32a9c9740f1d0c809c
Samuel Tijani <omalitijani@gmail.com>
2020-02-22 03:09:14 +0100

    Update file

--- e-SuperMartApp-FrontEnd/src/app/app.module.ts
+++ e-SuperMartApp-FrontEnd/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
+// Module imports
 import { AppRoutingModule } from "./app-routing.module";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppComponent } from "./app.component";
